de06d50945221698665c98588161fabcc7f5ccfe
cormick <cormick1080@gmail.com>
2021-04-10 15:18:36 +0800

    :sparkles: log cut by pre filename

--- logrotator.go
+++ logrotator.go
@@ -21,10 +21,26 @@ func NewTimeBasedRotator(pattern string, period time.Duration) (*TimeBasedRotato
 	return &tw, nil
 }
 
+func NewTimeBasedRotatorWithPreFilename(pattern string, period time.Duration, preFilename string) (*TimeBasedRotator, error) {
+	strfobj, err := strftime.New(pattern)
+	if err != nil {
+		return nil, errors.Wrap(err, `invalid time duration pattern`)
+	}
+
+	var tw TimeBasedRotator
+	tw.pattern = strfobj
+	tw.period = period.Nanoseconds()
+	_, offset := time.Now().Zone()
+	tw.timeDiffToUTC = (time.Duration(offset) * time.Second).Nanoseconds()
+	tw.preFilename = preFilename
+
+	return &tw, nil
+}
+
 func NewTimeBasedCleaner(pattern string, maxAge time.Duration) (*TimeBasedCleaner, error) {
 	var tc TimeBasedCleaner
 	tc.pattern = pattern
-	if maxAge < 0{
+	if maxAge < 0 {
 		return nil, errors.New(`maxAge must be greater than 0`)
 	}
 	tc.maxAge = maxAge
