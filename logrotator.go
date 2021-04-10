7bf609cfe24bc74025e1b11f4a1e5e910f69314b
cormick <cormick1080@gmail.com>
2021-04-10 14:29:48 +0800

    :tada: init projects

---
+++ logrotator.go
@@ -0,0 +1,33 @@
+package logrotator
+
+import (
+	"github.com/lestrrat/go-strftime"
+	"github.com/pkg/errors"
+	"time"
+)
+
+func NewTimeBasedRotator(pattern string, period time.Duration) (*TimeBasedRotator, error) {
+	strfobj, err := strftime.New(pattern)
+	if err != nil {
+		return nil, errors.Wrap(err, `invalid time duration pattern`)
+	}
+
+	var tw TimeBasedRotator
+	tw.pattern = strfobj
+	tw.period = period.Nanoseconds()
+	_, offset := time.Now().Zone()
+	tw.timeDiffToUTC = (time.Duration(offset) * time.Second).Nanoseconds()
+
+	return &tw, nil
+}
+
+func NewTimeBasedCleaner(pattern string, maxAge time.Duration) (*TimeBasedCleaner, error) {
+	var tc TimeBasedCleaner
+	tc.pattern = pattern
+	if maxAge < 0{
+		return nil, errors.New(`maxAge must be greater than 0`)
+	}
+	tc.maxAge = maxAge
+
+	return &tc, nil
+}
